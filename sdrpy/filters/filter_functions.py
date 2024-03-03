3d54e52d84b1d6816b5a5d29f59330ab8fcdbc77
Qavi Inan <qaviinan@gmail.com>
2024-03-03 20:28:43 +0600

    maturity completed

--- sdrpy/filters/filter_functions.py
+++ sdrpy/filters/filter_functions.py
@@ -1,22 +1,76 @@
 import pandas as pd
-from sdrpy.utils.util_functions import filter_by_currency
+from sdrpy.utils.util_functions import *
 
-def get_trades(data, product:str, currencies=None, product_type="FixedFloat", **kwargs):
-    if product == "xccy":
-        real_product = "InterestRate:CrossCurrency:"
-    product_name = str(real_product + product_type)
-    df = data[data["Product name"] == product_name]
-    
-    # Check if currencies is None or contains only one currency
+def get_trades (df, product="xccy", product_type="Basis", currencies="CAD", maturity="m>3", date_range="-1d"):
     if currencies is not None :
         if len(currencies) > 1:
-        
             df = filter_by_currency(df, *currencies)
-            
         else:     
-      
-            df = filter_by_currency(df, currencies)
-            
-       
-    
+            df = filter_currency(df, currencies)
+    print('currency filter returns;', len(df))
+    if product!=None or product_type!=None:
+        df = filter_product(df, product, product_type)
+    print('product filter returns;', len(df))
+    if maturity!=None:
+        df = filter_maturity(df, maturity)
+    #if date_range!=None:
+        #df = filter_date_range(df, date_range)
     return df
+
+def filter_product(df, product, product_type):
+    product_name_map = pd.read_csv('./sdrpy/data/product_name_map.csv', index_col=0)
+    try:
+        real_product_name = product_name_map.loc[product].real_name
+    except:
+        print('ERROR: Could not match product name! Please check data/product_name_map.csv for correct name mapping')
+    product_name = str(real_product_name + product_type)
+    print(product_name)
+    xdf = df[df["Product name"] == product_name]
+    return xdf
+
+def filter_currency(df, currency):
+    return df[(df["Notional currency-Leg 1"] == currency[0]) | (df["Notional currency-Leg 2"] == currency[0])]
+    #return df.loc[(df["Notional currency-Leg 1"]==currency) | (df["Notional currency-Leg 2"]==currency)]
+
+def filter_maturity(df, maturity_conditions):
+  """
+  Filters a dataframe based on the values in the "maturity" column.
+
+  Args:
+    df : dataframe to filter
+    maturity_conditions: string that can take several forms
+      "m>3" : simple one-sided bound
+      "3<m<6" : continuous range bound with smaller bound first [NOT 6>m>3]
+      "m<3, m>8" : Multiple mutually exclusive bounds
+
+  Returns:
+      A new pandas dataframe containing the filtered rows.
+  """
+  df5 = get_maturity_column(df)
+  bounds = extract_bounds(maturity_conditions)
+
+  if bounds[0]==1:
+      # Type 1 means single bound [(3, >)], so just use regular eval
+      for bound, operator in bounds[1:]:
+          print(operator, bound)
+          df5 = df5.loc[eval(f"df5['maturity'] {operator} {bound}")].copy()
+      overlap_df = df5
+  elif bounds[0]==2:
+      # Type 2 is continuous range [(3, '<'), (5, '<')], so merge separately
+      low_bound, low_operator = bounds[1]
+      low_df5 = df5.loc[eval(f"{low_bound} {low_operator} df5['maturity']")].copy()
+
+      hi_bound, hi_operator = bounds[2]
+      hi_df5 = df5.loc[eval(f"df5['maturity'] {hi_operator} {hi_bound}")].copy()
+      
+      overlap_df = custom_merge(low_df5, hi_df5, on='_id', how='inner')
+  elif bounds[0]==3:
+      # Type 3 is broken range [(3, '<'), (5, '>')], so concat separately
+      low_bound, low_operator = bounds[1]
+      low_df5 = df5.loc[eval(f"df5['maturity'] {low_operator} {low_bound}")].copy()
+
+      hi_bound, hi_operator = bounds[2]
+      hi_df5 = df5.loc[eval(f"df5['maturity'] {hi_operator} {hi_bound}")].copy()
+
+      overlap_df = pd.concat([low_df5, hi_df5], ignore_index=True).drop_duplicates()
+  return overlap_df
--- sdrpy/utils/util_functions.py
+++ sdrpy/utils/util_functions.py
@@ -1,3 +1,6 @@
+import re
+import pandas as pd
+
 def filter_by_currency(df, *currencies):
     """
     Filter DataFrame rows based on currencies.
@@ -13,3 +16,60 @@ def filter_by_currency(df, *currencies):
     filter_condition = df["Notional currency-Leg 1"].isin(currencies_list) | df["Notional currency-Leg 2"].isin(currencies_list)
     filtered_df = df[filter_condition]
     return filtered_df
+
+import re
+
+# To utils
+def get_maturity_column(df):
+    df["Effective Date"] = pd.to_datetime(df["Effective Date"])
+    df["Expiration Date"] = pd.to_datetime(df["Expiration Date"])
+    df["maturity"] = (df["Expiration Date"] - df["Effective Date"]).dt.days / 365
+
+    return df
+
+# To utils
+def extract_bounds(maturity_cond):
+  """
+  Extracts lower and upper bounds with their operators from a maturity condition string.
+
+  Args:
+      maturity_cond: A string representing the maturity condition (e.g., "3<m<30").
+
+  Returns:
+      A list of tuples containing (lower_bound, operator) and (upper_bound, operator).
+  """
+  
+  pattern1 = r"(?:^|\s+)(\d+)\s*([<>]=?)\s*m\s*([<>]=?)\s*(\d+)(?:$|\s+)" # match continuous range
+  pattern2 = r"m\s*([<>]=?)\s*(\d+)" # single bound
+  pattern3 = r"m\s*([<>]=?)\s*(\d+)\s*,\s*m\s*([<>]=?)\s*(\d+)" # match mutually-exclusive range
+
+  match1 = re.search(pattern1, maturity_cond)
+  match2 = re.search(pattern2, maturity_cond)
+  match3 = re.search(pattern3, maturity_cond)
+  
+  if match3:
+    lower_op, lower_bound, upper_op, upper_bound = map(str, match3.groups())
+    return [3, (int(lower_bound), lower_op), (int(upper_bound), upper_op)] # "m>5, m<3" returns [(5, '>'), (3, '<')]
+  elif match1:
+    lower_bound, lower_op, upper_op, upper_bound = map(str, match1.groups())
+    return [2, (int(lower_bound), lower_op), (int(upper_bound), upper_op)] # "3<m<5" returns [(3, '<'), (5, '<')]
+  elif match2:
+    return [1, (match2.groups()[1], match2.groups()[0])]  # "m>3" returns ('>', '3')
+
+# TO utils
+def custom_merge(left, right, on, how='inner'):
+  """
+  Merges DataFrames with custom suffix removal.
+
+  Args:
+      left: The left DataFrame.
+      right: The right DataFrame.
+      on: The column(s) to use for merging.
+      how: The merge method (default: 'inner').
+
+  Returns:
+      The merged DataFrame without suffixes.
+  """
+  merged_df = left.merge(right.copy(), on=on, how=how, suffixes=('', '_r'))
+  merged_df.columns = [col.rstrip('_r') for col in merged_df.columns]
+  return merged_df
